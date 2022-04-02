09ca319e723326f8bcd9147c2f9a5af5e0624d51
James Kelly <jameskelly@Jamess-MBP-2.local>
2022-04-02 00:11:29 -0700

    Begin work on planet control

---
+++ Assets/Scenes/James/Earth/EarthRotationControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotationControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [Range(0.0f, 15.0f)]
+    public float rotationSpeed = 1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X"), 0) * rotationSpeed);
+        }
+
+    }
+}
