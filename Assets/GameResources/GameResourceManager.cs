da29624d64db34d494623b72137c02fcabc5cb53
Jacob Ingalls <jacobingalls@yahoo.com>
2022-04-02 11:07:44 -0700

    Add Game Resource Manager

---
+++ Assets/GameResources/GameResourceManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResourceManager : MonoBehaviour
+{
+    
+    public int money = 1000;
+
+    public int scienceProduction = 1;
+
+    public int biomassConsumption = 5;
+
+    public int biomassProduction = 10;
+
+    public int energyConsumption = 5;
+
+    public int energyProduction = 5;
+
+    public int steelConsumption = 5;
+
+    public int steelProduction = 10;
+
+    public int titaniumConsumption = 5;
+
+    public int titaniumProduction = 10;
+
+    public Sprite biomassSprite, biomassSprite2x, biomassSprite3x;
+    public Sprite energySprite, energySprite2x, energySprite3x;
+    public Sprite moneySprite, moneySprite2x, moneySprite3x;
+    public Sprite scienceSprite, scienceSprite2x, scienceSprite3x;
+    public Sprite steelSprite, steelSprite2x, steelSprite3x;
+    public Sprite titaniumSprite, titaniumSprite2x, titaniumSprite3x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
