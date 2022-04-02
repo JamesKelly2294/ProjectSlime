71e1c7b0932741af6212cd8fb6d6593ea802000f
Jacob Ingalls <jacobingalls@yahoo.com>
2022-04-02 13:27:07 -0700

    Wire up Resources Display

--- Assets/UI/HUD/ResourcesDisplay.cs
+++ Assets/UI/HUD/ResourcesDisplay.cs
@@ -19,6 +19,19 @@ public class ResourcesDisplay : MonoBehaviour
     void Update()
     {
         money.value.text = gameResourceManager.money.ToString("#,0") + "B";
+        money.detail.text = gameResourceManager.moneyProduction.ToString("#,0") + "B per Year";
         
+        energy.value.text = gameResourceManager.energyProduction.ToString() + " / " + gameResourceManager.energyProduction.ToString();
+        if (gameResourceManager.energyProduction != 0) {
+            energy.detail.text = Mathf.RoundToInt(((float)gameResourceManager.energyCleanProduction / (float)gameResourceManager.energyProduction) * 100).ToString() + "% Clean Energy";
+            energy.detail.gameObject.SetActive(true);
+        } else {
+            energy.detail.gameObject.SetActive(false);
+        }
+
+        steel.value.text = gameResourceManager.steelConsumption.ToString() + " / " + gameResourceManager.steelProduction.ToString();
+        titanium.value.text = gameResourceManager.titaniumConsumption.ToString() + " / " + gameResourceManager.titaniumProduction.ToString();
+        biomass.value.text = gameResourceManager.biomassConsumption.ToString() + " / " + gameResourceManager.biomassProduction.ToString();
+        science.value.text = gameResourceManager.scienceProduction.ToString();
     }
 }
